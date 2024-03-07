10b3c0cf5e9a4a4ca3fce56d72f50ea7d18ed7bd
Yikai Zhou <zhou.yikai76@gmail.com>
2024-03-06 20:36:01 -0500

    setup boilerplate

---
+++ services/src/server.ts
@@ -0,0 +1,35 @@
+import express from 'express';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
+
+const port = 5001;
+
+// Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log("A user connected");
+  socket.emit('bruh')
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+
+  // Handle messages from clients
+  socket.on("message", (data) => {
+    console.log("Message received:", data);
+    // Implement your game logic here
+  });
+});
+
+// server.get("/", (req, res) => {
+//   console.log("req", req.query.data);
+//   res.send(req.query.data + " Hello World!");
+// });
+
+httpServer.listen(port, () => {
+  console.log(`app listening on port ${port}!`);
+});
